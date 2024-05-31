31d2e5157d82e566841a5964f6722a28de8063ed
Hisashi Miyashita <himi@mgnite.com>
2024-05-31 10:56:32 -0400

    Basic files are added.

---
+++ README.md
@@ -0,0 +1,9 @@
+# SysMLv2-Samples
+
+## About The Project
+
+This project is intended to demonstrate SysMLv2 modeling collaboration based on texutual editings.
+
+## Licenses
+
+Distributed under the LGPL. See `LICENSE` for more information.
---
+++ mg.cmd
@@ -0,0 +1,9 @@
+:: mg launch script template.
+@echo off
+:: If you want to configure MATLAB configuration here, uncomment and set your matlab.exe
+rem set MG_MATLAB_EXE_PATH=c:/Program Files/MATLAB/R2021a/bin/matlab.exe
+cd %~dp0
+:: Please set your Mg installation directory below
+call c:\Users\himi\git\Mg\build\Mg\launch.cmd
+
+
